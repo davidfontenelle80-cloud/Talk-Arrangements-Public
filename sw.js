301555a329a8e5223b3a5033811270a33c877b72
davidfontenelle80-cloud <davidfontenelle80@gmail.com>
2026-06-23 06:13:08 -0400

    Bump cache for fixed data model

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * BUMP THIS VERSION STRING on every deploy that changes HTML, CSS, JS, manifest, or SW behavior.
  */
 
-const CACHE_VERSION = 'talk-arrangements-v37-cleanup';
+const CACHE_VERSION = 'talk-arrangements-v38-fixed-data-model';
 
 /**
  * All URLs that make up the app shell.
